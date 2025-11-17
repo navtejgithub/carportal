27bad5d5503e4c2ed68cba9d55e7a7cee4fa6f92
Navtejp1 <navtejp1@gmail.com>
2025-11-17 12:36:26 +0530

    User Security Config JWT

--- src/main/java/com/carportal/controller/AuthController.java
+++ src/main/java/com/carportal/controller/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
